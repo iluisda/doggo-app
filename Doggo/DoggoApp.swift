4bf7ed27a641f7b9376d68547ce97738f2f0557d
Luis Alvarez <iluisdat@gmail.com>
2023-05-13 05:20:40 -0300

    Initial Commit

---
+++ Doggo/DoggoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoggoApp.swift
+//  Doggo
+//
+//  Created by Luis Alvarez on 13/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct DoggoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
