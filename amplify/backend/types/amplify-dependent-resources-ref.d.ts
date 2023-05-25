501053c6fc1c6aaff56a4f95b1d8f2f3c1ad983f
Luis Alvarez <iluisdat@gmail.com>
2023-05-25 19:35:48 -0300

    Amplify re configure

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,10 +1,12 @@
 export type AmplifyDependentResourcesAttributes = {
   "auth": {
-    "doggoa72566b9": {
+    "doggo95eb9d0a": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
       "IdentityPoolId": "string",
       "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
